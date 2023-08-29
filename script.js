0de4a2c1f151f21716e28bd30b9c747942145c1b
vuckoviic <vuckovica64@gmail.com>
2023-08-29 16:04:40 +0200

    Experimenting with colors

--- script.js
+++ script.js
@@ -17,3 +17,11 @@ const divide = function(a, b){
 const remainder = function(a, b){
     return a % b;
 }
+
+let num1 = 0;
+let operator = "";
+let num2 = 0;
+
+const operate = function(num1, opeator, num2){
+    
+}
